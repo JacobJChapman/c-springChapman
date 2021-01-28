7ef0fedf80e9cd1bfda854bc334ff9b2a1a19d10
JacobJChapman <78050387+JacobJChapman@users.noreply.github.com>
2021-01-28 09:49:52 -0800

    Update Problem 3 (employeepay).cpp

--- Assignment 1 JJameson/Problem 3 (employeepay).cpp
+++ Assignment 1 JJameson/Problem 3 (employeepay).cpp
@@ -6,25 +6,37 @@ using namespace std;
 IN MY CURRENT JOB I AM GIVEN AN ABSOLUTE MAXIMUM OF 40 HOURS A WEEK.*/
 
 int main() {
+  //set doubles
   double ratePay;
   ratePay = 16.00;
+  
   double overtimePay;
   overtimePay = ratePay * 1.50;
+  
   double ssTax;
   ssTax = 0.06;
+  
   double fedTax;
   fedTax = 0.14;
+  
   double stateTax;
   stateTax = 0.05;
+  
   double medInsurance;
   medInsurance = 10.0;
+  
   double hoursWorked;
   double overtimeHours;
   double moneyEarned;
   double takeHomePay;
   double withHeld;
+  
+  //used an if and else statement to determine whether overtime pay would be taken in
+  
   cout << "Enter hours worked:" << endl;
   cin >> hoursWorked;
+  
+  //chose to instead of creating integers to place in entire portions of the equations to simply type in the problems again
   if (hoursWorked > 40.0) {
     overtimeHours = hoursWorked - 40;
 
@@ -34,6 +46,7 @@ int main() {
 
     takeHomePay = ((40 * ratePay) + (overtimeHours * overtimePay)) * (1.0 - (ssTax + fedTax + stateTax)) - medInsurance;
   }
+  //non overtime pay
   else {
 
     moneyEarned = hoursWorked * ratePay;
@@ -42,6 +55,7 @@ int main() {
 
     takeHomePay = (hoursWorked * ratePay) * (1.0 - (ssTax + fedTax + stateTax)) - medInsurance;
   }
+  //now simply print the results
   cout << "Employee has earned $" << moneyEarned << "." << endl;
   cout << "$" << withHeld << " is withheld." << endl;
   cout << "Employee takes home $" << takeHomePay << "." << endl;

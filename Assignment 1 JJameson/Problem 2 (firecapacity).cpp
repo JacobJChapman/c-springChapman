cc043892ef1d5ccc10c70ccf21dc2f3d93817554
JacobJChapman <78050387+JacobJChapman@users.noreply.github.com>
2021-01-28 09:41:31 -0800

    Update Problem 2 (firecapacity).cpp

--- Assignment 1 JJameson/Problem 2 (firecapacity).cpp
+++ Assignment 1 JJameson/Problem 2 (firecapacity).cpp
@@ -6,11 +6,14 @@ int main() {
   int roomCapacity;
   int meetingAttendees;
   int exceedingCapacity;
+  //enter integers
   cout << "Enter the maximum room capacity for the meeting room:" << endl;
   cin >> roomCapacity;
   cout << "Per fire safety laws, the room can only hold " << roomCapacity << "." << endl;
   cout << "Enter how many attendees will be at the meeting:" << endl;
   cin >> meetingAttendees;
+  //find out the difference in attendees and capacity
+  //simply determine which is higher
   exceedingCapacity = meetingAttendees - roomCapacity;
   if (meetingAttendees > roomCapacity) {
     cout << "The meeting cannot commence legally with " << exceedingCapacity << " people exceeding capacity limit." << endl << "Therefore the meeting cannot start as planned." << endl;
